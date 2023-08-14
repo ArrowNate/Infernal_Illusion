a94ad0f07d8b04f708655094141cc68757ba30bc
ArrowNate <George.Edmon@triosstudent.com>
2023-08-14 00:25:45 -0400

    Programming The Hook Class

--- Infernal_Illusion/Source/Infernal_Illusion/Hook.cpp
+++ Infernal_Illusion/Source/Infernal_Illusion/Hook.cpp
@@ -2,6 +2,12 @@
 
 
 #include "Hook.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+#include "Components/PrimitiveComponent.h"
+#include "Logging/LogMacros.h"
+
+DEFINE_LOG_CATEGORY_STATIC(MyLogCategory, Log, All);
 
 // Sets default values for this component's properties
 UHook::UHook()
@@ -20,7 +26,9 @@ void UHook::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
+	GetComponentAndBind();
 	
+	UE_LOG(MyLogCategory, Warning, TEXT("YourFunction is being called!"));
 }
 
 
@@ -30,35 +38,77 @@ void UHook::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentT
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+
+	if (PhysicsHandle->GrabbedComponent) {
+		PhysicsHandle->SetTargetLocation(GetReachEnd());
+	}
+	UE_LOG(MyLogCategory, Warning, TEXT("YourFunction is being called!"));
 }
 
 void UHook::GetComponentAndBind()
 {
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
 
+	InputComponent->BindAction("Grab", IE_Pressed, this, &UHook::Grab);
+	InputComponent->BindAction("Grab", IE_Released, this, &UHook::Release);
+
+	UE_LOG(MyLogCategory, Warning, TEXT("YourFunction is being called!"));
 }
 
 void UHook::Grab()
 {
+	auto HitResult = GetPhysicsBodyInReach();
+	auto GrabComponent = HitResult.GetComponent();
+	auto ActorHit = HitResult.GetActor();
 
+	if (ActorHit) {
+		PhysicsHandle->GrabComponentAtLocation(GrabComponent, NAME_None, GrabComponent->GetOwner()->GetActorLocation());
+	}
+	UE_LOG(MyLogCategory, Warning, TEXT("YourFunction is being called!"));
 }
 
 void UHook::Release()
 {
-
+	PhysicsHandle->ReleaseComponent();
 }
 
 FHitResult UHook::GetPhysicsBodyInReach()
 {
+
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+
+	FHitResult HitResult;
+
+	GetWorld()->LineTraceSingleByObjectType(HitResult, GetReachStart(), GetReachEnd(), FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody), TraceParameters);
+
+	UE_LOG(MyLogCategory, Warning, TEXT("YourFunction is being called!"));
 	return FHitResult();
 }
 
 FVector UHook::GetReachStart()
 {
+
+	FVector PlayerViewPoint;
+	FRotator PlayerViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(PlayerViewPoint, PlayerViewPointRotation);
+
+	UE_LOG(MyLogCategory, Warning, TEXT("YourFunction is being called!"));
 	return FVector();
 }
 
 FVector UHook::GetReachEnd()
 {
-	return FVector();
+
+	FVector PlayerViewPoint;
+	FRotator PlayerViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(PlayerViewPoint, PlayerViewPointRotation);
+
+	FVector LineTraceEnd = PlayerViewPoint + PlayerViewPointRotation.Vector() * Reach;
+
+	UE_LOG(MyLogCategory, Warning, TEXT("YourFunction is being called!"));
+	return LineTraceEnd;
 }
 
